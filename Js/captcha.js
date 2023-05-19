65bd3ea4f59ec59a05bbd95594d4f74321680172
estefania <shizukakumi@gmail.com>
2023-05-19 04:21:07 -0300

    tpe 'terminado'

--- Js/captcha.js
+++ Js/captcha.js
@@ -1,29 +1,29 @@
 "use strict";
 
-//declaramos variables
-let textCaptcha = document.querySelector("#txt-random");//<p></p>
-let input = document.querySelector("#captcha-form");//<input>
-let btn = document.querySelector("#comprobar");//<button></button>
-let verificacionText = document.querySelector("#verificacion");//<p>Incorrecto</p>
 
-btn.addEventListener("click", comprobar_captcha);//evento de "click" en el botón
+let textCaptcha = document.querySelector("#txt-random");
+let input = document.querySelector("#captcha-form");
+let btn = document.querySelector("#comprobar");
+let verificacionText = document.querySelector("#verificacion");
 
-let numeroRandom = Math.floor((Math.random()*9999)+1);//genera número al azar
+btn.addEventListener("click", comprobar_captcha);
+
+let numeroRandom = Math.floor((Math.random()*9999)+1);
 
 function crear_numero() {
-    textCaptcha.innerHTML = numeroRandom;//agregar al texto tu numero random
+    textCaptcha.innerHTML = numeroRandom;
 }
 
 crear_numero();
 
 function comprobar_captcha() {
-    if (numeroRandom == input.value) {//si numeroRandom es igual al valor del input
+    if (numeroRandom == input.value) {
         verificacionText.innerHTML = "Correcto.";
         verificacionText.classList.add("ok");
         verificacionText.classList.remove("fail");
-    }else{//sino
+    }else{
         verificacionText.innerHTML = "Incorrecto.";
         verificacionText.classList.add("fail");
         verificacionText.classList.remove("ok");
     }
-}//:)
+}
