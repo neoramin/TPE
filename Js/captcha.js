3fb4b432e32762324857e9f63ef970616f28f743
fabian <fabian.romero.ignacio@gmail.com>
2023-06-11 17:29:11 -0300

    tabla dinamica

--- Js/captcha.js
+++ Js/captcha.js
@@ -27,3 +27,70 @@ function comprobar_captcha() {
         verificacionText.classList.remove("ok");
     }
 }
+
+//tabla dinamica//
+
+
+document.querySelector("#btn-agregar").addEventListener("click", agregar);
+document.querySelector("#btn-vaciartabla").addEventListener("click", vaciartabla);
+document.querySelector("#btn-agregar3").addEventListener("click", agregar3);
+
+let nombres = [{ "artista": artista, },
+
+{ "cancion": cancion },
+{ "rate": rate }];
+
+nombres = [{ "artista": "artista" },
+
+{ "cancion": cancion }, { "rate": " rate" }];
+
+let usuario = [];
+//agrega elementos
+function agregar() {
+  console.log("Funcion Agregar");
+
+  let artista = document.querySelector('#artista').value;
+  let cancion = document.querySelector('#cancion').value;
+  let rate = document.querySelector('#rate').value;
+  let renglon = {
+    "artista": artista,
+    "cancion": cancion,
+    "rate": rate
+  }
+  usuario.push(renglon);
+
+  let tabla = document.querySelector("#tabla");
+  tabla.innerHTML = "";
+  tabla.innerHTML = tabla.innerHTML + " <tr>" + "<th>" + "Artista" + "</th>" + "<th>" + "Cancion" + "</th>" + "<th>" + "Rate" + "</th>" + "</tr>";
+  
+  usuario.forEach(renglon => {
+
+
+    tabla.innerHTML +=
+      `<tr>
+   <td>${renglon.artista}
+  </td>
+   <td>${renglon.cancion}
+  </td>
+  <td>${renglon.rate} %
+  </td>
+ </tr>` ;
+  }
+  );
+
+}
+//vacia la tabla
+function vaciartabla() {
+  let tabla = document.querySelector("#tabla");
+  tabla.innerHTML = "";
+  tabla.innerHTML = tabla.innerHTML + " <tr>" + "<th>" + "artista" + "</th>" + "<th>" + "artista" + "</th>" + "<th>" + "cancion" + "</th>" + "</tr>";
+}
+//agrega 3 elementos 
+
+function agregar3() {
+  for (let i = 0; i < 3; i++) {
+    agregar();
+
+  }
+
+}
